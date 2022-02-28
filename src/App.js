f4d461fab9617349b239efad54b149e9ae66a48b
yoshiioko <acardoza@pdx.edu>
2022-02-27 18:05:59 -0800

    Update: implement event listener to capture when NFT is minted.

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 const OPENSEA_LINK = "";
 const TOTAL_MINT_COUNT = 50;
 
+const CONTRACT_ADDRESS = "0xA99f9f8BEb591cE4b1622B465e1Ea55f94F201EB";
+
 const App = () => {
   /*
    * Define state vars to use for our wallet information
@@ -42,6 +44,10 @@ const App = () => {
       const account = accounts[0];
       console.log("Found an authorized account:", account);
       setCurrentAccount(account);
+
+      // Setup listener! Comes up if user comes to site and already had their
+      // wallet connected and authorized
+      setupEvenListener();
     } else {
       console.log("No authorized account found");
     }
@@ -71,6 +77,42 @@ const App = () => {
        */
       console.log("Connected", accounts[0]);
       setCurrentAccount(accounts[0]);
+
+      // Setup listener! This is for the case where a user comes to our site
+      // and connected their wallet for the first time.
+      setupEvenListener();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // Setup our listener
+  const setupEvenListener = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(
+          CONTRACT_ADDRESS,
+          myEpicNft.abi,
+          signer
+        );
+
+        // This is the magic sause
+        // This will capture our event when our contract throws it
+        connectedContract.on("NewEpicNTFMinted", (from, tokenId) => {
+          console.log(from, tokenId.toNumber());
+          alert(
+            `Hey there! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`
+          );
+        });
+
+        console.log("Setup even listener!");
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
     } catch (error) {
       console.log(error);
     }
@@ -80,8 +122,6 @@ const App = () => {
    * Call our contract to mint an NFT
    */
   const askContractToMintNft = async () => {
-    const CONTRACT_ADDRESS = "0xA99f9f8BEb591cE4b1622B465e1Ea55f94F201EB";
-
     try {
       const { ethereum } = window;
 
